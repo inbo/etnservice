9adfad4ce089ce449fd90399d9f2c96c1b47766b
PietrH <pieterhuy@gmail.com>
2023-02-06 10:58:04 +0100

    add package level documentation

---
+++ R/etnservice-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr %>%
+#' @importFrom dplyr .data
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr pull
+## usethis namespace: end
+NULL
