01670fad72fc5cdbd430589edefbb56972be8a0c
PietrH <pieterhuy@gmail.com>
2023-02-08 17:08:55 +0100

    use passed credentials only

--- tests/testthat/test-connect_to_etn.R
+++ tests/testthat/test-connect_to_etn.R
@@ -1,11 +1,15 @@
-test_that("connect_to_etn() allows to create a connection with default parameters", {
-  con <- connect_to_etn()
-  expect_true(check_connection(con))
-  expect_true(isClass(con, "PostgreSQL"))
-})
+# test_that("connect_to_etn() allows to create a connection with default parameters", {
+#   con <- connect_to_etn()
+#   expect_true(check_connection(con))
+#   expect_true(isClass(con, "PostgreSQL"))
+# })
 
-test_that("connect_to_etn() allows to create a connection with specific parameters", {
-  con <- connect_to_etn(Sys.getenv("userid"), Sys.getenv("pwd"))
-  expect_true(check_connection(con))
-  expect_true(isClass(con, "PostgreSQL"))
+test_that("connect_to_etn() allows to create a connection with passed credentials", {
+  con <- list(
+    username = Sys.getenv("userid"),
+    password = Sys.getenv("pwd")
+  )
+  connection <- connect_to_etn(con$username,con$password)
+  expect_true(check_connection(connection))
+  expect_true(isClass(connection, "PostgreSQL"))
 })
