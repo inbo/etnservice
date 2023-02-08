e62186f72a2a81c8cb49e6f7a8b26491cb1eaaa2
PietrH <pieterhuy@gmail.com>
2023-02-08 13:56:53 +0100

    con contains credentials, not the connection

--- tests/testthat/test-list_animal_project_codes.R
+++ tests/testthat/test-list_animal_project_codes.R
@@ -1,4 +1,7 @@
-con <- connect_to_etn()
+con <- list(
+  username = Sys.getenv("userid"),
+  password = Sys.getenv("pwd")
+)
 
 test_that("list_animal_project_codes() returns unique list of values", {
   vector <- list_animal_project_codes(con)
