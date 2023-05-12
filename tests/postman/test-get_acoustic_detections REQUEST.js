91341b2cf67d690c4d6a1215a4e4ad6c7f0c7c43
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-12 12:04:54 +0200

    requests for 2 part (eg RDA/console output) protocol requests

---
+++ tests/postman/test-get_acoustic_detections REQUEST.js
@@ -0,0 +1,12 @@
+pm.test("Status code is 201", function () {
+    pm.response.to.have.status(201);
+  });
+
+responsePaths = pm.response.text();
+savedData = responsePaths.split("\n")[0];
+savedData = savedData.slice(6)
+pm.collectionVariables.set("savedData", savedData);
+
+pm.test("Response time is less than 3s", function () {
+    pm.expect(pm.response.responseTime).to.be.below(3000);
+  });
