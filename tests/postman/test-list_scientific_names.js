7364f8b85f2f7f1026fa897357c05ed771833f85
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-05-12 12:02:04 +0200

    change reference list of scientific names with db change

--- tests/postman/test-list_scientific_names.js
+++ tests/postman/test-list_scientific_names.js
@@ -7,9 +7,10 @@ pm.test("returns the right animal ids", () => {
       //array is not empty
       pm.expect(jsonData).to.not.be.empty;
       //ids include number of known values
-      pm.expect(jsonData).to.include.members(["Dactylopterus volitans","Galeocerdo cuvier","Salmo trutta","Alosa fallax","Palinurus elephas","Torpedo torpedo","Solea senegalensis","Symphodus melops","Mobula tarapacana","Pollachius pollachius","Carcharhinus albimarginatus","Caranx ignobilis","Raja asterias","Salvelinus alpinus","Argyrosomus regius","Diplodus vulgaris","Balistes capriscus","Petromyzon marinus","Pomatomus saltatrix","Anguilla angulla","Pagellus bogaraveo","Galeorhinus galeus","Pseudocaranx dentex","Hexanchus griseus","Carcharhinus leucas","Diplodus sargus","Salmo t. trutta","Anguilla anguilla","Sphyraena barracuda","Sphyraena viridensis","Nebrius ferrugineus","Rostroraja alba","Chelon ramada","Limanda limanda","Seriola dumerili ","Salmo salar","Scyliorhinus stellaris","Scyllarides latus","Oncorhynchus mykiss","Seriola rivoliana","Sander lucioperca","Test testium","Labrus bergylta","Coryphaena hippurus"," Salmo trutta","Thunnus thynnus","Scyliorhinus canicula","Built-in","Dentex dentex","Mustelus sp."]);
+      pm.expect(jsonData).to.include.members(["Alosa fallax", "Anguilla anguilla", "Argyrosomus regius", "Balistes capriscus", "Built-in", "Caranx ignobilis", "Carcharhinus albimarginatus", "Carcharhinus leucas", "Chelon ramada", "Coryphaena hippurus", "Dactylopterus volitans", "Dentex dentex", "Diplodus sargus", "Diplodus vulgaris", "Galeocerdo cuvier", "Galeorhinus galeus", "Hexanchus griseus", "Labrus bergylta", "Limanda limanda", "Mobula tarapacana", "Mustelus sp.", "Nebrius ferrugineus", "Oncorhynchus mykiss", "Pagellus bogaraveo", "Palinurus elephas", "Petromyzon marinus", "Pollachius pollachius", "Pomatomus saltatrix", "Pseudocaranx dentex", "Raja asterias", "Rostroraja alba", "Salmo salar", "Salmo t. trutta", "Salmo trutta", "Salvelinus alpinus", "Sander lucioperca", "Scyliorhinus canicula", "Scyliorhinus stellaris", "Scyllarides latus", "Seriola dumerili", "Seriola rivoliana", "Solea senegalensis", "Sphyraena barracuda", "Sphyraena viridensis", "Symphodus melops", "Test testium", "Thunnus thynnus", "Torpedo torpedo"]);
   });
 
 pm.test("Response time is less than 3s", function () {
     pm.expect(pm.response.responseTime).to.be.below(3000);
   });
+
