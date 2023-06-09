faf228be3cc7dc657f9e5371bd626fac8cd156cc
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-09 16:00:45 +0200

    adapt `list_deployment_ids()` for use by OpenCPU

--- tests/testthat/test-list_deployment_ids.R
+++ tests/testthat/test-list_deployment_ids.R
@@ -1,7 +1,10 @@
-con <- connect_to_etn()
+credentials <- list(
+  username = Sys.getenv("userid"),
+  password = Sys.getenv("pwd")
+)
 
 test_that("list_deployment_ids() returns unique list of values", {
-  vector <- list_deployment_ids(con)
+  vector <- list_deployment_ids(credentials)
 
   expect_is(vector, "character")
   expect_false(any(duplicated(vector)))
