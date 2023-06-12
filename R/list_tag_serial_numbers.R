2b1c658dbccb76c815e31a67b68a61a49f58ffd3
PietrH <pieterhuy@gmail.com>
2023-06-12 10:24:58 +0200

    adapt `list_tag_serial_numbers()` for use by OpenCPU

--- R/list_tag_serial_numbers.R
+++ R/list_tag_serial_numbers.R
@@ -1,12 +1,21 @@
 #' List all available tag serial numbers
 #'
-#' @param connection A connection to the ETN database. Defaults to `con`.
+#' @param credentials A list with the username and password to connect to the ETN database.
 #'
 #' @return A vector of all unique `tag_serial_numbers` present in
 #'   `common.tag_device`.
 #'
 #' @export
-list_tag_serial_numbers <- function(connection = con) {
+list_tag_serial_numbers <- function(credentials = list(
+  username = Sys.getenv("userid"),
+  password = Sys.getenv("pwd")
+)) {
+  # Create connection object
+  connection <- connect_to_etn(credentials$username, credentials$password)
+
+  # Check if we can make a connection
+  check_connection(connection)
+
   query <- glue::glue_sql(
     "SELECT DISTINCT serial_number FROM common.tag_device",
     .con = connection
