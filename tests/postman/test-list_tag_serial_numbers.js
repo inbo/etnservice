078b2a120dc47572d1f6cfe3ccc2441b4b6225cf
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-06-22 13:19:29 +0200

    update postman tests

---
+++ tests/postman/test-list_tag_serial_numbers.js
@@ -0,0 +1,8 @@
+pm.test("Status code is 201", function () {
+    pm.response.to.have.status(201);
+  });
+
+pm.test("Response time is less than 3s", function () {
+      pm.expect(pm.response.responseTime).to.be.below(3000);
+  });
+
