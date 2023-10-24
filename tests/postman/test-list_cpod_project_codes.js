2437f840d93d215608e5c88d89e0e6c1afb28606
Pieter Huybrechts <48065851+PietrH@users.noreply.github.com>
2023-10-24 10:59:05 +0200

    add test with response content expectation

--- tests/postman/test-list_cpod_project_codes.js
+++ tests/postman/test-list_cpod_project_codes.js
@@ -6,3 +6,24 @@ pm.test("Response time is less than 3s", function () {
       pm.expect(pm.response.responseTime).to.be.below(3000);
   });
 
+const jsonData = pm.response.json();
+pm.test("returns the right cpod project codes", () => {
+      //array is not empty
+      pm.expect(jsonData).to.not.be.empty;
+      //ids include number of known values
+      pm.expect(jsonData).to.include.members([
+    "Apelafico_acoustics",
+    "Apelafico_underwater",
+    "cpod-lifewatch",
+    "cpod-od-natuur",
+    "PAM-Borssele",
+    "PelFish",
+    "PhD_Parcerisas",
+    "SEAWave",
+    "SMGMIT",
+    "STRAITS_PAM",
+    "VLIZ-MRC-AMUC-001",
+    "VLIZ-MRC-AMUC-002",
+    "WaveHub"
+]);
+  });
