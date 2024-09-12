cc6dcc041e46e14bc09b12cfaa62da5e7c1f94bf
PietrH <pieterhuy@gmail.com>
2024-09-12 10:40:21 +0200

    update postman tests

--- tests/postman/test-get_tags.js
+++ tests/postman/test-get_tags.js
@@ -2,6 +2,6 @@ pm.test("Status code is 201", function () {
     pm.response.to.have.status(201);
   });
 
-pm.test("Response time is less than 6s", function () {
-    pm.expect(pm.response.responseTime).to.be.below(6000);
+pm.test("Response time is less than 7.5s", function () {
+    pm.expect(pm.response.responseTime).to.be.below(7500);
   });
--- tests/postman/test-list_animal_project_codes.js
+++ tests/postman/test-list_animal_project_codes.js
@@ -7,10 +7,10 @@ pm.test("returns the right animal project codes", () => {
       //array is not empty
       pm.expect(jsonData).to.not.be.empty;
       //ids include number of known values
-      pm.expect(jsonData).to.include.members(["GIBRALTRACK_pilot","FISHINTEL","LamYorOus18-20","SMUCC","2016_Diaccia_Botrona","2015_fint","2013_albertkanaal","Rijke_Noordzee","mepnsw","VMLSOCBS","ASMOP2","Top-Predator","2014_Nene","Noordzeekanaal","2015_phd_verhelst_eel","CESB","Fish_Mig_Wad_Sea","2015_phd_verhelst_cod","MIGRATOEBRE","RNP","SARTELZINGARO","VVV","SVNL-WS","MBA_Massmo","Skye","FISHOWF","RAS","MICHIMIT","V2LNR","PTN/PROTECT2012/whiteseabream","PLASTIBE","2015_Albertkanaal_VPS_Ham","SU.MO.ELASMO.Adriatic","2021_Gudena","BFTDK","OP-Test","amsterdam","BTN-DeepWater-IMEDEA","KBTN_FISH","2012_leopoldkanaal","SEMP","BlueCrab2022Algarve","Eel-source-to-sea","SwanseaSeaTroutAdult","kornwerderzand","2015_dijle","codnoise","CONNECT-MED","FISHGAL","2013_Foyle"]);
+      pm.expect(jsonData).to.include.members(["GIBRALTRACK_pilot","FISHINTEL","LamYorOus18-20","SMUCC","2016_Diaccia_Botrona","2015_fint","2013_albertkanaal","Rijke_Noordzee","mepnsw","VMLSOCBS","ASMOP2","2014_Nene","Noordzeekanaal","2015_phd_verhelst_eel","CESB","Fish_Mig_Wad_Sea","2015_phd_verhelst_cod","MIGRATOEBRE","RNP","SARTELZINGARO","VVV","SVNL-WS","MBA_Massmo","Skye","FISHOWF","RAS","MICHIMIT","V2LNR","PTN/PROTECT2012/whiteseabream","PLASTIBE","2015_Albertkanaal_VPS_Ham","SU.MO.ELASMO.Adriatic","2021_Gudena","BFTDK","OP-Test","amsterdam","BTN-DeepWater-IMEDEA","KBTN_FISH","2012_leopoldkanaal","SEMP","BlueCrab2022Algarve","Eel-source-to-sea","SwanseaSeaTroutAdult","kornwerderzand","2015_dijle","codnoise","CONNECT-MED","FISHGAL","2013_Foyle"]);
   });
 
-pm.test("Response time is less than 3s", function () {
-    pm.expect(pm.response.responseTime).to.be.below(3000);
+pm.test("Response time is less than 5s", function () {
+    pm.expect(pm.response.responseTime).to.be.below(5000);
   });
 
