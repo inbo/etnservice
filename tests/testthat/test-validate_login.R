1746ab0e8bf3b3d6b6b70d427b0c0c23ab9a55ab
PietrH <pieterhuy@gmail.com>
2025-01-15 13:22:54 +0100

    Add function to check if credentials can be used to login

---
+++ tests/testthat/test-validate_login.R
@@ -0,0 +1,32 @@
+test_that("validate_login() returns error on missing arguments", {
+  # password missing
+  expect_error(
+    validate_login(username = "a username"),
+    regexp = "No password provided.",
+    fixed = TRUE
+  )
+  # username missing
+  expect_error(
+    validate_login(password = "a password",
+                   regexp = "No username provided.",
+                   fixed = TRUE)
+  )
+  # both missing
+  expect_error(
+    validate_login()
+  )
+})
+
+test_that("validate_login() returns TRUE on a unsuccesful login attempt", {
+  expect_false(
+    validate_login("username",
+                   "the wrong password")
+  )
+})
+
+test_that("validate_login() returns TRUE on a succesful login attempt", {
+  expect_true(
+    validate_login(Sys.getenv("userid"),
+                   Sys.getenv("pwd"))
+  )
+})
