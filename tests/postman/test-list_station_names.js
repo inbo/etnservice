98e72828551d09e4680f800148a881c4d49e6068
PietrH <pieterhuy@gmail.com>
2025-03-04 15:05:31 +0100

    Update test-list_station_names.js

--- tests/postman/test-list_station_names.js
+++ tests/postman/test-list_station_names.js
@@ -7,7 +7,7 @@ pm.test("returns the right station names", () => {
       //array is not empty
       pm.expect(jsonData).to.not.be.empty;
       //ids include number of known values
-      pm.expect(jsonData).to.include.members(["76 127 w                                                    ","84-PONTA-CEDROS-FUNDO","101 CORVO","ABDN Bay-4","APPA E","bpns-HD17","C2","CoastNet_PTN_Tejo_071","D1.11","F22","F36","FSUS","G4","GARD5","gm_2017_13","gn-14","HVALPSUND5","I27","IMFSTP019","IMFSTP035","L5","Ler15","LOM28","MR 23","NB014","NB028","Nene25","ngOudm","O32","OM 3","Pedra do Leao","PLANIER_EST","PREVOST_MER","PTN_#78","R 15","r02","Rt2","s-12","SB14","SN_E12_C","Sound6","VB14","Vossemeer"]);
+      pm.expect(jsonData).to.include.members(["above-kaunas-2","below-kaunas-3","klaipeda-4","Rusne-3","klaipeda-3","klaipeda-2","Rusne-4","Rusne-2","below-kaunas-2","above-kaunas-4","Sound2","Sound5","Sound4","Sound1","Sound6","Sound7","Sound3","Sound8","11","4","5","10","2","8","7","9","3","St08","St02","St25","St04","St19","St16","St27","St22","St23","St28","U/S Wetherby Weir ","U/S Naburn weir lhb","U/S Crakehill gauging weir","U/S Cromwell","D/S Linton weir right bank","U/S Swale conf","D/S Boroughbridge Weir","Maunby or Morton Bridge","D/S Kirk Hammerton","18","16","15","14","LANAYE","KINKEMPOIS","ANHEE","FREYR","DINANT","NAMUR","KEIZERSVEERBRUG_LB","we-ak-5","MOOK"]);
   });
 
 pm.test("Response time is less than 3s", function () {
