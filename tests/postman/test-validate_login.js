b797995b1e2305788ccb52533b92ee1612ff4ce9
PietrH <pieterhuy@gmail.com>
2025-03-27 15:16:24 +0100

    Increase time limit, add test for validation success

--- tests/postman/test-validate_login.js
+++ tests/postman/test-validate_login.js
@@ -1,8 +1,10 @@
 pm.test("Status code is 201", function () {
     pm.response.to.have.status(201);
-  });
-  
-pm.test("Response time is less than 9s", function () {
-      pm.expect(pm.response.responseTime).to.be.below(9000);
-  });
+});
 
+pm.test("Response time is less than 15s", function () {
+    pm.expect(pm.response.responseTime).to.be.below(15000);
+});
+pm.test("Response is exactly [true]", function () {
+    pm.expect(pm.response.json()).to.eql([true]);
+});
