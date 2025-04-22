5a348fde4d2ea1b7a0f657d03bd37cccd8bad677
PietrH <pieterhuy@gmail.com>
2025-04-22 15:45:13 +0200

    Only query once per function test context.

--- tests/testthat/test-list_receiver_ids.R
+++ tests/testthat/test-list_receiver_ids.R
@@ -4,22 +4,30 @@ credentials <- list(
 )
 
 test_that("list_receiver_ids() returns unique list of values", {
-  receiver_ids <- list_receiver_ids(credentials)
+  if (!exists("receiver_ids")) {
+    receiver_ids <- list_receiver_ids(credentials)
+  }
   expect_false(any(duplicated(receiver_ids)))
 })
 
 test_that("list_receiver_ids() returns a character vector", {
-  receiver_ids <- list_receiver_ids(credentials)
+  if (!exists("receiver_ids")) {
+    receiver_ids <- list_receiver_ids(credentials)
+  }
   expect_type(receiver_ids, "character")
 })
 
 test_that("list_receiver_ids() does not return NA values", {
   skip("Empty receiver value in acoustic.receivers, ISSUE https://github.com/inbo/etn/issues/333")
-  receiver_ids <- list_receiver_ids(credentials)
+  if (!exists("receiver_ids")) {
+    receiver_ids <- list_receiver_ids(credentials)
+  }
   expect_true(all(!is.na(receiver_ids)))
 })
 
 test_that("list_receiver_ids() returns known value", {
-  receiver_ids <- list_receiver_ids(credentials)
+  if (!exists("receiver_ids")) {
+    receiver_ids <- list_receiver_ids(credentials)
+  }
   expect_true("VR2W-124070" %in% receiver_ids)
 })
