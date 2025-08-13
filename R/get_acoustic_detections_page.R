18db0326f9d3c6638dde31456699661409786bdd
PietrH <pieterhuy@gmail.com>
2025-08-13 14:23:45 +0200

    Stub to fetch next page of new view

---
+++ R/get_acoustic_detections_page.R
@@ -0,0 +1,29 @@
+#' Getting a single page of a multi page acoustic detections query
+#'
+get_acoustic_detections_page <- function(next_id_pk = 0,
+                                         page_size = 1000000) {
+  # Check if credentials object has right shape
+  check_credentials(credentials)
+
+  # Create connection object
+  connection <- connect_to_etn(credentials$username, credentials$password)
+
+  # Check if we can make a connection
+  check_connection(connection)
+
+  # Build the query to fetch the next page
+  query <- glue::glue_sql("
+    SELECT
+      det.detection_id_pk FROM acoustic.detections_animal AS det
+    WHERE {start_date_query}
+      AND {end_date_query}
+      AND {acoustic_tag_id_query}
+      AND {animal_project_code_query}
+      AND {scientific_name_query}
+      AND {acoustic_project_code_query}
+      AND {receiver_id_query}
+      AND {station_name_query}
+      AND det.detection_id_pk > {next_id_pk}
+    LIMIT {page_size}
+    ", .con = connection)
+}
