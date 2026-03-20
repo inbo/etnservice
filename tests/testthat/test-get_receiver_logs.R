c20a8e76768f526f484534eb52f4ee766f111cf7
pieter.huybrechts@inbo.be <pieter.huybrechts@inbo.be@rstudio4.web.vliz.be>
2026-03-20 10:41:43 +0000

    Move relevant tests over from etn.

--- tests/testthat/test-get_receiver_logs.R
+++ tests/testthat/test-get_receiver_logs.R
@@ -3,5 +3,56 @@ test_that("get_receiver_logs() returns a tibble", {
 })
 
 test_that("get_receiver_logs() returns the expected columns", {
+  expected_column_names <- c(
+    "deployment_id",
+    "receiver_id",
+    "datetime",
+    "record_type",
+    "log_data"
+  )
 
+  expect_named(get_receiver_logs(deployment_id = test_deployment_id,
+                                 limit = TRUE),
+               expected_column_names
+  )
+
+})
+
+test_that("get_receiver_logs() returns the expected column classes", {
+
+})
+
+test_that("get_receiver_logs() returns no duplicate rows", {
+  expect_identical(
+    get_receiver_logs(deployment_id = test_deployment_id),
+    dplyr::distinct(get_receiver_logs(deployment_id = test_deployment_id))
+  )
+})
+
+test_that("get_receiver_logs() returns a 0-row tibble if no receiver logs found", {
+
+})
+
+test_that("get_receiver_logs() can filter on deployment_id", {
+
+})
+
+test_that("get_receiver_logs() can filter on receiver_id", {
+
+})
+
+test_that("get_receiver_logs() can filter on start_date", {
+
+})
+
+test_that("get_receiver_logs() can filter on end_date", {
+
+})
+
+test_that("get_receiver_logs() can return a limited subset", {
+  # This test assumes that there are more than 100 logs for the test deployment
+  expect_length(
+    get_receiver_logs(test_deployment_id, limit = TRUE)[1],
+    100L
+  )
 })
